9d845265d053cd14650f8232335a3dac00dc68a2
Sunny Park <dlstjs8246@naver.com>
2020-01-07 15:20:12 +0900

    예제9-2 추가

---
+++ src/java_swing_study/chap09/ContentPaneEx.java
@@ -0,0 +1,26 @@
+package java_swing_study.chap09;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ContentPaneEx extends JFrame {
+	public ContentPaneEx() {
+		setTitle("contentPane과 JFrame");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container contentPane = getContentPane();
+		contentPane.setBackground(Color.orange);
+		contentPane.setLayout(new FlowLayout());
+		
+		contentPane.add(new JButton("OK"));
+		contentPane.add(new JButton("Cancel"));
+		contentPane.add(new JButton("Ignore"));
+		
+		setSize(300,150);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new ContentPaneEx();
+	}
+}
--- src/java_swing_study/chap09/SwingMain.java
+++ src/java_swing_study/chap09/SwingMain.java
@@ -1,9 +1,20 @@
 package java_swing_study.chap09;
 
+import java.awt.EventQueue;
+
 public class SwingMain {
 
 	public static void main(String[] args) {
-		MyFrame mf = new MyFrame();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ContentPaneEx2 frame = new ContentPaneEx2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
